64209652978cdf42ab95bb4bd2561bd90fc4f2f0
Omer Berger <oberger@liveperson.com>
2018-05-17 11:25:52 -0400

    setup ABC button

--- ABC_ChatButton/ViewController.swift
+++ ABC_ChatButton/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+/** ABC Chat Button & Action implementation:
+ 1. Add BusinessChat framework
+ 2. Get your account business id
+ 3. Create button with a target action
+ */
+import BusinessChat
 
+let bid = "<ADD_YOUT_BUSINESS_ID_HERE>"
+
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Making sure we running @min. OS requirements:
+        if #available(iOS 11.3, *) {
+            createABCButton(style:.light, frame: CGRect.zero)
+        }else{
+            //Fallback condition here
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func createABCButton(style: BCChatButton.Style, frame: CGRect) {
+        let abcButton   =  BCChatButton(style: style)
+        abcButton.addTarget(self, action: #selector(buttonTapped(chatButton:)), for: UIControlEvents.touchUpInside)
+        abcButton.frame = frame
+        view.addSubview(abcButton)
     }
-
-
+    
+    @objc func buttonTapped(chatButton: BCChatButton) {
+        let parameters: [BCChatAction.Parameter : String] = [
+            .intent: "<DESIRED_INTENT_HERE>",
+            .group: "<DESIRED_GROUP_HERE>",
+            .body: "<DESIRED_BODY_HERE>"
+        ]
+        
+        BCChatAction.openTranscript(businessIdentifier: bid, intentParameters: parameters)
+    }
+    
 }
 
